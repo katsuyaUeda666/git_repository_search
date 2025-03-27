d28971e5fc1aea180de0331091b29e8491176f39
かつ <uedakatsuya@ramenojisan.local>
2025-03-27 22:18:58 +0900

    ver2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:git_repository_search/pages/homePage.dart';
+import 'package:git_repository_search/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
