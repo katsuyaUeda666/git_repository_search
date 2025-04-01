25636a26d23d687e7f39ebe25caeab7a3e2b7a50
かつ <uedakatsuya@ramenojisan.local>
2025-04-01 22:42:43 +0900

    検索の状態管理追加

---
+++ lib/controller/search_notifier.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'search_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$searchNotifierHash() => r'1ccda215a0fb94c9d063e5f12b20c399e7cec520';
+
+/// See also [SearchNotifier].
+@ProviderFor(SearchNotifier)
+final searchNotifierProvider =
+    AsyncNotifierProvider<SearchNotifier, List<Repository>>.internal(
+  SearchNotifier.new,
+  name: r'searchNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$searchNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SearchNotifier = AsyncNotifier<List<Repository>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
